c5d9bb3167a2464eea9d636acb770a15a11811d6
Nomicron <thenomicron@gmail.com>
2025-04-25 16:10:10 +0200

    Added PlayerLook and updated PlayerMove script

--- Script/Player/PlayerLook.cs
+++ Script/Player/PlayerLook.cs
@@ -4,5 +4,25 @@ using System;
 public partial class PlayerLook : Node
 {
     [Export] private CharacterBody3D player;
-    [Export] private float mouseSensitivity;
+    [Export] private Node3D head;
+    [Export] private float mouseSensitivity = 0.1f;
+    [Export] private float minXRotation = -45;
+    [Export] private float maxXRotation = 50;
+
+    private Vector2 lookInput;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion eventMouseMotion)
+        {
+            player.RotateY(Mathf.DegToRad(-eventMouseMotion.Relative.X * mouseSensitivity));
+            head.RotateX(Mathf.DegToRad(eventMouseMotion.Relative.Y * mouseSensitivity));
+            head.Rotation = Mathf.Clamp(head.Rotation.X, Mathf.DegToRad(minXRotation), Mathf.DegToRad(maxXRotation)) * Vector3.Right;
+        }
+    }
 }
--- Script/Player/PlayerMove.cs
+++ Script/Player/PlayerMove.cs
@@ -4,107 +4,135 @@ using System;
 public partial class PlayerMove : Node
 {
 	[Export] private CharacterBody3D player;
-	[Export] private Node3D head;
-	[Export] private float speed = 5.0f;
-	[Export] private float jumpVelocity = 4.5f;
+	[Export] private float aheadSpeed = 3.25f;
+	[Export] private float backSpeed = 2.75f;
+	[Export] private float strafeSpeed = 2.5f;
+	[Export] private float sprintModifier = 1.5f;
+	[Export] private float crouchModifier = 0.5f;
+	[Export] private float jumpVelocity = 4.0f;
 
 	private Vector3 velocity;
 	private Vector2 moveInput;
-	private Vector3 direction;
 
-    //Update synced with program framerate
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-    }
+	private float currentSpeedModifiers;
 
 	//Update synced with physics
 	public override void _PhysicsProcess(double delta)
 	{
-		velocity = player.Velocity;
+		ResetVariables();
 
-		direction = Vector3.Zero;
+		UpdateMoveInput();
 
-		UpdateDirection();
+		ApplyGravity(delta);
 
-		UpdateGravity(delta);
+		ApplyJump();
 
-		UpdateJump();
+		ApplySprint();
+
+		ApplyCrouch();
 
 		CalculateVelocity();
 
 		player.MoveAndSlide();
 	}
 
-    //Get movement direction from input
-    private void UpdateDirection()
-    {
-	    if (Input.IsActionPressed("MoveAhead"))
+	//Resets variables to default values each frame synced with physics
+	private void ResetVariables()
+	{
+		velocity = player.Velocity;
+
+		moveInput = Vector2.Zero;
+
+		currentSpeedModifiers = 1.0f;
+	}
+
+	//Get movement direction from input
+	private void UpdateMoveInput()
+	{
+		if (Input.IsActionPressed("MoveAhead"))
 		{
-			moveInput.Y = 1;
+			moveInput.Y = -1;
 
 			GD.Print("MoveAhead");
 		}
 
-	    if (Input.IsActionPressed("MoveBack"))
+		if (Input.IsActionPressed("MoveBack"))
 		{
-			moveInput.Y = -1;
+			moveInput.Y = 1;
 
 			GD.Print("MoveBack");
 		}
 
-	    if (Input.IsActionPressed("MoveLeft"))
+		if (Input.IsActionPressed("MoveLeft"))
 		{
 			moveInput.X = 1;
 
 			GD.Print("MoveLeft");
 		}
 
-	    if (Input.IsActionPressed("MoveRight"))
+		if (Input.IsActionPressed("MoveRight"))
 		{
 			moveInput.X = -1;
 
 			GD.Print("MoveRight");
 		}
-    }
+
+		if (moveInput.Length() > 1)
+			moveInput = moveInput.Normalized();
+	}
 
 	//Apply gravity to character
-	private void UpdateGravity(double delta)
-    {
+	private void ApplyGravity(double delta)
+	{
 		if (!player.IsOnFloor())
 		{
 			velocity += player.GetGravity() * (float)delta;
 		}
-    }
+	}
 
-	//Apply jump force when action is pressed 
-    private void UpdateJump()
-    {
+	//Apply jump force when action is activated 
+	private void ApplyJump()
+	{
 		if (Input.IsActionJustPressed("Jump") && player.IsOnFloor())
 		{
 			velocity.Y = jumpVelocity;
 		}
-    }
+	}
+
+	//Apply sprint speed when action is activated
+	private void ApplySprint()
+	{
+		if (Input.IsActionPressed("Sprint") && player.IsOnFloor() && moveInput.Y < 0 && !Input.IsActionPressed("Crouch"))
+		{
+			currentSpeedModifiers *= sprintModifier;
+
+			GD.Print("Sprint");
+		}
+	}
+
+	//Apply crouch speed and hitbox when action is activated
+	private void ApplyCrouch()
+	{
+		if (Input.IsActionPressed("Crouch") && player.IsOnFloor() && !Input.IsActionPressed("Sprint"))
+		{
+			currentSpeedModifiers *= crouchModifier;
+
+			GD.Print("Crouch");
+		}
+	}
 
 	//Calculate and apply velocity to character
-    private void CalculateVelocity()
-    {
-		direction = Vector3.Zero;
+	private void CalculateVelocity()
+	{
+		Vector3 forwardMove = Vector3.Forward * moveInput.Y * (moveInput.Y >= 0 ? aheadSpeed : backSpeed) * currentSpeedModifiers;
+		Vector3 strafeMove = Vector3.Right * moveInput.X * strafeSpeed * currentSpeedModifiers;
 
-		direction = (player.Transform.Basis * new Vector3(moveInput.X, 0, moveInput.Y)).Normalized();
+		Vector3 localMove = forwardMove + strafeMove;
 
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * speed;
-			velocity.Z = direction.Z * speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(player.Velocity.X, 0, speed);
-			velocity.Z = Mathf.MoveToward(player.Velocity.Z, 0, speed);
-		}
+		Vector3 globalMove = player.Transform.Basis * localMove;
+
+		velocity = new Vector3(globalMove.X, velocity.Y, globalMove.Z);
 
 		player.Velocity = velocity;
-    }
-
+	}
 }
